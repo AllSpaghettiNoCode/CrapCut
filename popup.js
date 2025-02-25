28e23836993108d27b738710eea0247bd0af6408
DrGear <79474598+DrGear@users.noreply.github.com>
2025-02-25 00:41:30 -0400

    add feature to clear list of saved audios

--- popup.js
+++ popup.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const downloadList = document.getElementById("downloadList");
+    const clearHistoryBtn = document.getElementById("clearHistoryBtn");
 
     chrome.runtime.sendMessage({ action: "getDownloadHistory" }, (response) => {
         const history = response.history || [];
 
         if (history.length === 0) {
-            downloadList.innerHTML = "<li>No downloads here yet.</li>";
+            downloadList.innerHTML = "<li>No downloads yet.</li>";
         } else {
             history.forEach(url => {
                 const listItem = document.createElement("li");
                 const link = document.createElement("a");
                 link.href = url;
-                link.textContent = "Download.mp3";
+                link.textContent = "Download";
                 link.target = "_blank";
                 listItem.appendChild(link);
                 downloadList.appendChild(listItem);
             });
         }
+    });
+
+    clearHistoryBtn.addEventListener("click", () => {
+        chrome.runtime.sendMessage({ action: "clearDownloadHistory" }, () => {
+            downloadList.innerHTML = "<li>No downloads yet.</li>";
+        });
     });
 });
